e1eb2db6a791970bf11be17235a575a4763032b7
Allah Rakha <armozi07911000@gmail.com>
2025-07-25 00:26:45 +0500

    Add routes in app and api setup

--- backend/app.js
+++ backend/app.js
@@ -1,54 +1,43 @@
-const express = require("express");
-const ErrorHandler = require("./middleware/error");
-const app = express();
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import express from 'express';
+import { errorHandler } from './utils/errorHandler.js';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
-app.use(cors({
-  origin: ['https://eshop-tutorial-pyri.vercel.app',],
-  credentials: true
-}));
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/test", (req, res) => {
-  res.send("Hello world!");
-});
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-// config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({
-    path: "config/.env",
-  });
+  dotenv.config();
 }
 
-// import routes
-const user = require("./controller/user");
-const shop = require("./controller/shop");
-const product = require("./controller/product");
-const event = require("./controller/event");
-const coupon = require("./controller/coupounCode");
-const payment = require("./controller/payment");
-const order = require("./controller/order");
-const conversation = require("./controller/conversation");
-const message = require("./controller/message");
-const withdraw = require("./controller/withdraw");
+import user from "./controller/user.js";
+import shop from "./controller/shop.js";
+import product from "./controller/product.js";
+import event from "./controller/event.js";
+import coupon from "./controller/coupounCode.js";
+import payment from "./controller/payment.js";
+import order from "./controller/order.js";
+import conversation from "./controller/conversation.js";
+import message from "./controller/message.js";
+import withdraw from "./controller/withdraw.js";
 
-app.use("/api/v2/user", user);
-app.use("/api/v2/conversation", conversation);
-app.use("/api/v2/message", message);
-app.use("/api/v2/order", order);
-app.use("/api/v2/shop", shop);
-app.use("/api/v2/product", product);
-app.use("/api/v2/event", event);
-app.use("/api/v2/coupon", coupon);
-app.use("/api/v2/payment", payment);
-app.use("/api/v2/withdraw", withdraw);
+app.use("/api/v1/user", user);
+app.use("/api/v1/conversation", conversation);
+app.use("/api/v1/message", message);
+app.use("/api/v1/order", order);
+app.use("/api/v1/shop", shop);
+app.use("/api/v1/product", product);
+app.use("/api/v1/event", event);
+app.use("/api/v1/coupon", coupon);
+app.use("/api/v1/payment", payment);
+app.use("/api/v1/withdraw", withdraw);
 
-// it's for ErrorHandling
-app.use(ErrorHandler);
 
-module.exports = app;
+app.use(errorHandler);
+
+export default app;
