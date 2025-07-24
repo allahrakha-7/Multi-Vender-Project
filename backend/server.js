bbf6af08242fb39b07203b7e90901f216981030d
Allah Rakha <armozi07911000@gmail.com>
2025-07-25 00:34:13 +0500

    Add server and complete functionalities

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,17 @@
-// import { app } from './app.js';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app.js';
+import { v2 as cloudinary } from 'cloudinary';
 
 
-// process.on("uncaughtException", (err) => {
-//   console.log(`Error: ${err.message}`);
-//   console.log(`Shutting down the server for handling uncaught exception`);
-//   process.exit(1);
-// });
-
-// mongoose.connect(process.env.MONGO)
-//   .then(() => console.log(`Connected to MongoDB!`))
-//   .catch((err) => console.log(err));
-
-
-// const server = app.listen(process.env.PORT, () => {
-//   console.log(
-//     `Server is running on http://localhost:${process.env.PORT}`
-//   );
-// });
-
-
-// process.on("unhandledRejection", (err) => {
-//   console.log(`Shutting down the server for ${err.message}`);
-//   console.log(`Shutting down the server for unhandled promise rejection`);
-  
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
-
-
-const app = require("./app");
-const connectDatabase = require("./db/Database");
-const cloudinary = require("cloudinary");
-
-// Handling uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`shutting down the server for handling uncaught exception`);
 });
 
-// config
+
 if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({
-    path: "config/.env",
-  });
+  dotenv.config();
 }
 
 mongoose.connect(process.env.MONGO, {
@@ -52,21 +19,21 @@ mongoose.connect(process.env.MONGO, {
   useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB!')).catch((err) => console.log(err));
 
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
-})
+});
 
 
-// create server
 const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server is running on http://localhost:${process.env.PORT}`
   );
 });
 
-// unhandled promise rejection
+
 process.on("unhandledRejection", (err) => {
   console.log(`Shutting down the server for ${err.message}`);
   console.log(`shutting down the server for unhandle promise rejection`);
