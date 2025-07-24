dd7ca7b317507b4591bb9d20a09c4f1c02400dae
Allah Rakha <armozi07911000@gmail.com>
2025-07-24 15:53:52 +0500

    Add middleware and functionalities

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,17 +1,17 @@
-const ErrorHandler = require("../utils/ErrorHandler");
-const catchAsyncErrors = require("./catchAsyncErrors");
-const jwt = require("jsonwebtoken");
-const User = require("../model/user");
-const Shop = require("../model/shop");
+import { errorHandler } from "../utils/errorHandler.js";
+import catchAsyncError from "./catchAsyncError.js";
+import User from '../model/user.model.js';
+import Shop from '../model/shop.model.js';
+import jwt from 'jsonwebtoken';
 
-exports.isAuthenticated = catchAsyncErrors(async(req,res,next) => {
-    const {token} = req.cookies;
+export const isAuthenticated = catchAsyncError(async (req,res,next) => {
+    const {access_token} = req.cookies;
 
     if(!token){
-        return next(new ErrorHandler("Please login to continue", 401));
+        return next(new errorHandler("Please login to continue", 401));
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const decoded = jwt.verify(access_token, process.env.JWT_SECRET_KEY);
 
     req.user = await User.findById(decoded.id);
 
@@ -19,10 +19,10 @@ exports.isAuthenticated = catchAsyncErrors(async(req,res,next) => {
 });
 
 
-exports.isSeller = catchAsyncErrors(async(req,res,next) => {
+export const isSeller = catchAsyncError(async (req,res,next) => {
     const {seller_token} = req.cookies;
     if(!seller_token){
-        return next(new ErrorHandler("Please login to continue", 401));
+        return next(new errorHandler("Please login to continue", 401));
     }
 
     const decoded = jwt.verify(seller_token, process.env.JWT_SECRET_KEY);
@@ -33,10 +33,10 @@ exports.isSeller = catchAsyncErrors(async(req,res,next) => {
 });
 
 
-exports.isAdmin = (...roles) => {
+export const isAdmin = (...roles) => {
     return (req,res,next) => {
         if(!roles.includes(req.user.role)){
-            return next(new ErrorHandler(`${req.user.role} can not access this resources!`))
+            return next(new errorHandler(`${req.user.role} can not access this resources!`))
         };
         next();
     }
--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,31 +1,27 @@
-const ErrorHandler = require("../utils/ErrorHandler");
+import { errorHandler } from '../utils/errorHandler.js'
 
-module.exports = (err, req, res, next) => {
+export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server Error";
 
-  // wrong mongodb id error
   if (err.name === "CastError") {
     const message = `Resources not found with this id.. Invalid ${err.path}`;
-    err = new ErrorHandler(message, 400);
+    err = new errorHandler(message, 400);
   }
 
-  // Duplicate key error
   if (err.code === 11000) {
     const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
-    err = new ErrorHandler(message, 400);
+    err = new errorHandler(message, 400);
   }
 
-  // wrong jwt error
   if (err.name === "JsonWebTokenError") {
     const message = `Your url is invalid please try again letter`;
-    err = new ErrorHandler(message, 400);
+    err = new errorHandler(message, 400);
   }
 
-  // jwt expired
   if (err.name === "TokenExpiredError") {
     const message = `Your Url is expired please try again letter!`;
-    err = new ErrorHandler(message, 400);
+    err = new errorHandler(message, 400);
   }
 
   res.status(err.statusCode).json({
